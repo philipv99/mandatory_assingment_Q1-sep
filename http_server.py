e5ccbd7cbdd31524534faa258721da40d406f262
philipv99 <philipv9913@gmail.com>
2025-09-05 10:10:28 +0200

    refactor of kode

--- http_server.py
+++ http_server.py
@@ -6,50 +6,40 @@ server_socket.bind(("", server_port))
 server_socket.listen(1)
 
 RESPONSE_CODES = {200: "200 OK", 400: "400 bad request", 404: "404 not found"}
+HTTP_VERSIONS = ["HTTP/1", "HTTP/1.1"]
 
 print("server is running.")
 
-def host_string(hs:str) -> bool:
+def http_host_acceptet(hs:str) -> bool:
   if "Host: www.python.com\r\n" in hs:
-    print("Hostname: Approved")
     return True
   else:
-    print("Hostname: Approved")
+    print("Hostname: denied")
     return False
   
-  
-def request_line_end_check(end:str):
-  if end == "HTTP/1\r\n" or end == "HTTP/1.1\r\n":
-    return True
-  else: 
-    return False
-  
+def send_response_to_client(response:str) -> None:
+  Connection_socket.send(response.encode())
+  Connection_socket.close()
 
-def request_line(qry:str):
-  qry = qry.split(' ')
-  file_to_send = ""
-
-  if qry[0] != 'GET':
-    return RESPONSE_CODES[400]
-
-  if not request_line_end_check(qry[2]):
-    return RESPONSE_CODES[400]
-  
-  return fill_operation(qry[1])
+def http_request_line_check(qry:str) -> list:
+  get, path, version = qry.split(' ') #split http -> 0:[get] 1:[/...] 2:[http/1.1\r\n]
+  if get != 'GET':
+    send_response_to_client(server_HTTP_response(400, "operation not autoherised"))
+  if version in HTTP_VERSIONS:
+    send_response_to_client(server_HTTP_response(400, "version not supported"))
+  return path
   
   
-def fill_operation(qry:str):
-  fill_qry = qry
-  if qry == '/':
-    fill_qry = "/index.html"
+def http_request_fill_retrive(path:str) -> str|None:
+  path = path if path != '/' else "/index.html"
   try:
-    file_object = open(fill_qry.removeprefix('/'), "r")
+    file_object = open(path.removeprefix('/'), "r")
     response_data = file_object.read()
     file_object.close()
-    return server_HTTP_response(200, response_data)
   except:
-    print(RESPONSE_CODES[404])
-    return RESPONSE_CODES[404]
+    send_response_to_client(server_HTTP_response(404))
+    return
+  return response_data
 
 
 def server_HTTP_response(code:int, data:str=None) -> str:
@@ -60,24 +50,22 @@ def server_HTTP_response(code:int, data:str=None) -> str:
     re = re + data + "\r\n\r\n"
   return re
 
-
 #server stat
 while True:
   Connection_socket, addr = server_socket.accept()
-  server_response = ""
-  print(addr)
   message = Connection_socket.recv(2048)
-  # to with request?
+  print(addr, "connected")
 
-  http_request = message.decode().split('\n')
-  for i, v in enumerate(http_request):
-    http_request[i] = http_request[i] + '\n'
-  http_request.pop(3)
-
-  
-  if host_string(http_request[1]) and http_request[2] == '\r\n':
-    server_response = request_line(http_request[0])
-  print(server_response)
-
-  Connection_socket.send(server_response.encode())
-  Connection_socket.close()
+  try:
+    http_request, http_host, http_request_newline, line = message.decode().split('\n')
+    http_request, http_host, http_request_newline = http_request + '\n', http_host + '\n', http_request_newline + '\n'
+    if http_host_acceptet(http_host) and http_request_newline == '\r\n':
+      http_get_request = http_request_line_check(http_request)
+      http_request_data = http_request_fill_retrive(http_get_request)
+    send_response_to_client(server_HTTP_response(200, http_request_data))
+  except: 
+    send_response_to_client(server_HTTP_response(400))
+    print("400 response send")
+  finally:
+    print(addr, "connection closed")
+    
